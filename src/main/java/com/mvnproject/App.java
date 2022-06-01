d8c41a52689de19ac08830ca457e65c2ddf28654
Your Name <you@example.com>
2022-06-01 16:06:58 +0000

    commiting src and pom.xml file

---
+++ src/main/java/com/mvnproject/App.java
@@ -0,0 +1,13 @@
+package com.mvnproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
