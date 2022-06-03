44b08b1b1c570611cfda21beeec4c02dbbde70ee
Your Name <you@example.com>
2022-06-03 07:04:48 +0000

    auto run again

--- src/main/java/com/mvnproject/App.java
+++ src/main/java/com/mvnproject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! again chages in cli and automate the jenkins pipeline");
+        System.out.println( "Hello World! again chages in cli and automate the jenkins pipeline autorun");
     }
 }
